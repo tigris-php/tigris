5d3bb684899c945ee986386d84f8849af8bc1087
Alexey Samoylov <alexey.samoylov@gmail.com>
2016-05-23 18:54:51 +0700

    added InlineQuery type

---
+++ src/Types/Inline/InlineQuery.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+namespace Tigris\Types\Inline;
+
+use Tigris\Types\Base\BaseObject;
+use Tigris\Types\Location;
+use Tigris\Types\Scalar\ScalarInteger;
+use Tigris\Types\Scalar\ScalarString;
+use Tigris\Types\User;
+
+/**
+ * Class InlineQuery
+ *
+ * @package Tigris\Types
+ * @link https://core.telegram.org/bots/api#inlinequery
+ *
+ * @property ScalarInteger $id
+ * @property User $from
+ * @property Location $location
+ * @property ScalarString $query
+ * @property ScalarString $offset
+ */
+class InlineQuery extends BaseObject
+{
+    /**
+     * @inheritdoc
+     */
+    protected static function fields()
+    {
+        return [
+            'id' => ScalarString::class,
+            'from' => User::class,
+            'location' => Location::class,
+            'query' => ScalarString::class,
+            'offset' => ScalarString::class,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected static function requiredFields()
+    {
+        return [
+            'id',
+            'from',
+        ];
+    }
+}
