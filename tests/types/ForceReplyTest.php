7a9c474660ff070fa9811104b9a0765023f0fd3f
Alexey Samoylov <alexey.samoylov@gmail.com>
2016-06-19 18:45:06 +0700

    updated force reply test

--- tests/types/ForceReplyTest.php
+++ tests/types/ForceReplyTest.php
@@ -3,12 +3,14 @@
  * @author Alexey Samoylov <alexey.samoylov@gmail.com>
  */
 use Tigris\Types\ForceReply;
+use Tigris\Types\Interfaces\ReplyMarkupInterface;
 
 class ForceReplyTest extends PHPUnit_Framework_TestCase
 {
     public function testCreate()
     {
         $a = ForceReply::create(true);
+        $this->assertInstanceOf(ReplyMarkupInterface::class, $a);
         $this->assertInstanceOf(ForceReply::class, $a);
         $this->assertSame(true, $a->force_reply->value, $a);
         $this->assertSame(true, $a->selective->value, $a);
