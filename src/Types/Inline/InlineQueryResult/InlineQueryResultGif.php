f7a5073809551a9f0ba81f0bacf130d13e028e2d
Alexey <alexey.samoylov@gmail.com>
2016-10-12 14:24:25 +0700

    added InlineQueryResultGif

---
+++ src/Types/Inline/InlineQueryResult/InlineQueryResultGif.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author Alexey Samoylov <alexey.samoylov@gmail.com>
+ */
+namespace Tigris\Types\Inline\InlineQueryResult;
+
+use Tigris\Types\Scalar\ScalarBoolean;
+use Tigris\Types\Scalar\ScalarInteger;
+use Tigris\Types\Scalar\ScalarString;
+
+/**
+ * Represents a link to an animated GIF file.
+ * By default, this animated GIF file will be sent by the user with optional caption.
+ * Alternatively, you can use input_message_content to send a message with the specified content instead of the animation.
+ *
+ * @package Tigris\Types\Inline
+ * @link https://core.telegram.org/bots/api#inlinequeryresultgif
+ *
+ * @property string $gif_url A valid URL for the GIF file. File size must not exceed 1MB.
+ * @property integer $gif_width Optional. Width of the GIF.
+ * @property integer $gif_height Optional. Height of the GIF.
+ * @property string $thumb_url URL of the static thumbnail for the result (jpeg or gif).
+ * @property string $title Optional. Title for the result.
+ * @property string $caption Optional. Caption of the GIF file to be sent, 0-200 characters.
+ */
+class InlineQueryResultGif extends AbstractInlineQueryResult
+{
+    const TYPE = 'gif';
+
+    protected static function extraFields()
+    {
+        return [
+            'gif_url' => ScalarString::class,
+            'gif_width' => ScalarInteger::class,
+            'gif_height' => ScalarInteger::class,
+            'thumb_url' => ScalarString::class,
+            'title' => ScalarString::class,
+            'caption' => ScalarString::class,
+        ];
+    }
+}
