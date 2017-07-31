8a09635a516def5f6802dac8befdb63525aab19f
Oleg <theorder83@gmail.com>
2017-07-31 14:58:51 +0700

    fix test

--- tests/sessions/SQLiteSessionFactoryTest.php
+++ tests/sessions/SQLiteSessionFactoryTest.php
@@ -8,10 +8,7 @@ class SQLiteSessionFactoryTest extends PHPUnit_Framework_TestCase
 {
     private function getDbPath()
     {
-        $path =  sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'test1.sql';
-        @unlink(@$path);
-
-        return $path;
+        return sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'test1.sql';
     }
 
     public function testConstruct()
